56d63029e80558b076fccff305c2518d2c40c89e
Yi Wei <genius.ron@gmail.com>
2016-03-09 18:40:49 +0800

    research recover

--- gofa.go
+++ gofa.go
@@ -8,6 +8,8 @@ import (
 	"net/url"
 	"flag"
 	"strconv"
+	"net/http"
+	"io/ioutil"
     "github.com/parnurzeal/gorequest"
 )
 
@@ -22,7 +24,8 @@ func main() {
 	fmt.Println(id, num)
 	done := make(chan bool, num)
 	for i := 0; i < num; i++ {
-		go geti(id, done)
+		//go geti(id, done)
+		go ti(id, done)
 	}
 	for i := 0; i < num; i++ {
 	    <-done
@@ -34,6 +37,13 @@ func main() {
 
 func geti(id int, done chan bool) {
 	fmt.Println("start geti")
+	defer func(){ // 必须要先声明defer，否则不能捕获到panic异常
+        fmt.Println("c")
+        if err:=recover();err!=nil{
+            fmt.Println(err) // 这里的err其实就是panic传入的内容，55
+        }
+        fmt.Println("d")
+    }()
 	defer fmt.Println("end geti")
 	template := "http://events.chncpa.org/wmx2016/action/pctou.php?id=%d&user_ip=%s&time=%s"
 	s1 := rand.NewSource(time.Now().UnixNano())
@@ -60,6 +70,44 @@ func geti(id int, done chan bool) {
 		return
 	} else {
 		fmt.Println(body)
+	}
+	
+	done <- true
+}
+
+func ti(id int, done chan bool) {
+	fmt.Println("start geti")
+	defer fmt.Println("end geti")
+	defer func() {
+        if r := recover(); r != nil {
+            fmt.Println("Recovered in f", r)
+        }
+    }()
+	template := "http://events.chncpa.org/wmx2016/action/pctou.php?id=%d&user_ip=%s&time=%s"
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	ip := fmt.Sprintf("%d.%d.%d.%d", r1.Intn(250)+1, r1.Intn(250)+1, r1.Intn(250)+1, r1.Intn(250)+1)
+	t := time.Now()
+	ts := t.Add(-24*time.Hour)
+	//fmt.Println(url.QueryEscape(ts.Format("2006-01-02 15:04:05")))
+	dtime := url.QueryEscape(ts.Format("2006-01-02 15:04:05")) //"2016-02-26+28%3A35%3A38"
+	url := fmt.Sprintf(template, id, ip, dtime)
+	//url = "http://wtfismyip.com/text"
+	fmt.Println(url)
+	panic(1)
+    client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	req.Header.Set("Referer", "http://events.chncpa.org/wmx2016/mobile/pages/jmpx.php")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405")
+	res, errs := client.Do(req)
+	if errs != nil {
+		log.Fatal(errs)
+
+	} else {
+		body, _ := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		fmt.Println(string(body))
 	}
 	
 	done <- true
